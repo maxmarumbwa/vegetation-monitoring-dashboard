d1db5296e21b21e4c21321926749f0e9cf9b0d79
maxmarumbwa <maxmarumbwa@gmail.com>
2026-05-10 14:55:45 +0200

    Fix production settings

--- GreenMonitor_ZW/settings.py
+++ GreenMonitor_ZW/settings.py
@@ -18,13 +18,10 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     "CSRF_TRUSTED_ORIGINS", "https://geostat-agro-monitor.onrender.com"
