6c8af160f9813d37248f63b532b83c0039c75d28
maxmarumbwa <maxmarumbwa@gmail.com>
2026-05-10 14:52:43 +0200

    Fix production settings

--- GreenMonitor_ZW/settings.py
+++ GreenMonitor_ZW/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS", "localhost,127.0.0.1,xxxxx.onrender.com"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS", "https://geostat-agro-monitor.onrender.com"
 ).split(",")
 
 # Application definition
